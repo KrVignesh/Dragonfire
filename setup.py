35f056d084a7c5ae238236384f627e1b67ff1d40
KrVigneshVictor <krvigneshchn17@gmail.com>
2018-06-03 21:24:08 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -91,7 +91,7 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python3 :: Only',
     ],
 
     # What does your project relate to?
